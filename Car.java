39546565d6d4b242fe1962bf6301c584017e94cd
VladKrasii <59770952+VladKrasii@users.noreply.github.com>
2020-01-26 12:06:19 +0200

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,18 @@
+    public abstract class Car {
+
+
+    String model = "Model: Q7";
+    String color = "Color: Green";
+    String speed = "MaxSpeed: 325 km/h";
+
+
+    void gas  () {
+        System.out.println("gas:" + 100);
+    }
+
+    void brake () {
+        System.out.println("brake: " + 20);
+    }
+    abstract void description();
+}
+
